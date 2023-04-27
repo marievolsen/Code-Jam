0072e082eddd593dd7475d31485276ae0691091f
Victor Hejø <vhejo22@student.aau.dk>
2023-04-27 16:07:18 +0200

    Code Corrections

--- Code Jam Project/Assets/Scripts/AccelerometerController.cs
+++ Code Jam Project/Assets/Scripts/AccelerometerController.cs
@@ -4,40 +4,35 @@ using UnityEngine;
 
 public class AccelerometerController : MonoBehaviour
 {
-    [SerializeField] float speed;
+    //Based on "BoxScript" and "Respawn", made by Victor Hejø during HTX at ZBC Ringsted.
+    //Based on https://www.youtube.com/watch?v=fsEkZLBeTJ8
 
-
-    Rigidbody rb;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+    [SerializeField] private int yValue;
+    public static Vector3 spawnposition;
+    [SerializeField] private Vector3 startPosition;
+    public static int checkpointCount;
+    public static int collectibleCount;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if (checkpointCount == 0)
+        {
+            spawnposition = startPosition;
+        }
+        transform.position = spawnposition;
     }
 
     void FixedUpdate()
     {
         Vector3 acc = Input.acceleration;
-        rb.AddForce(acc.x * speed, 0, acc.y * speed);
+        rb.AddForce(acc.x * speed, yValue, acc.y * speed);
     }
 
-    //Based on https://www.youtube.com/watch?v=fsEkZLBeTJ8
-    /*public bool isFlat = true;
-    private Rigidbody rigid;
-
-    private void Start()
+    public void SetSpawnPosition(Vector3 position)
     {
-        rigid = GetComponent<Rigidbody>();
+        spawnposition = position;
     }
-
-    private void Update()
-    {
-        Vector3 tilt = Input.acceleration;
-
-        if (isFlat)
-        {
-            tilt = Quaternion.Euler(90, 0, 0) * tilt;
-        }
-
-        rigid.AddForce(tilt);
-    }*/
 }
