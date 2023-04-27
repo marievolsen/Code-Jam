1aa995580bf771c515e998a16755f533f2529672
Victor Hejø <vhejo22@student.aau.dk>
2023-04-27 14:30:11 +0200

    Brigde Level
    Created a bridge level with moving platforms.

--- Code Jam Project/Assets/Scripts/Old Game Scripts/MovingPlatformControls.cs
+++ Code Jam Project/Assets/Scripts/Old Game Scripts/MovingPlatformControls.cs
@@ -5,22 +5,18 @@ public class MovingPlatformControls : MonoBehaviour {
 
 	public GameObject startPos;
 	public GameObject endPos;
-	
 	public float speed = 3f;
-	
 	public bool goForEnd = true;
 	
-	// Update is called once per frame
-	void Update () {
-		if (goForEnd) {
-			Debug.Log("go for end " + goForEnd);
+	void Update ()
+	{
+		if (goForEnd)
+		{
 			transform.Translate ((endPos.transform.position-transform.position).normalized * speed * Time.deltaTime);
 			if(Vector3.Distance(transform.position, endPos.transform.position) <= 1.0f){
-				Debug.Log(Vector3.Distance(transform.position, endPos.transform.position) <= 1.0f);
 				goForEnd = false;
 			}
 		}else {
-			Debug.Log("go for end " + goForEnd);
 			transform.Translate ((startPos.transform.position-transform.position).normalized * speed * Time.deltaTime);
 			if(Vector3.Distance(transform.position, startPos.transform.position) <= 1.0f){
 				goForEnd = true;
