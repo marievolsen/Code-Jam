bfcd5fde247f746c22b61d052d03d5d52dbc4e55
Victor Hejø <vhejo22@student.aau.dk>
2023-05-03 20:21:04 +0200

    Spring Cleaning Part 2

---
+++ Code Jam Project/Assets/Scripts/MovingPlatformControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformControls : MonoBehaviour {
+
+	public GameObject startPos;
+	public GameObject endPos;
+	public float speed;
+	public bool goForEnd = true;
+	[SerializeField] private float proximity;
+
+	void Update ()
+	{
+		if (goForEnd)
+		{
+			transform.Translate ((endPos.transform.position-transform.position).normalized * speed * Time.deltaTime);
+			if(Vector3.Distance(transform.position, endPos.transform.position) <= proximity){
+				goForEnd = false;
+			}
+		}else {
+			transform.Translate ((startPos.transform.position-transform.position).normalized * speed * Time.deltaTime);
+			if(Vector3.Distance(transform.position, startPos.transform.position) <= proximity){
+				goForEnd = true;
+			}
+		}
+	}
+}
